ce0bb1b72794507d37be480bb4f87dabbe377d66
BBMAG\kapablpl <blazej.kapalka_ext@bbraun.com>
2024-05-20 12:15:37 +0200

    started working on album component

--- Frontend-Internship-Challange/src/app/album/album.component.ts
+++ Frontend-Internship-Challange/src/app/album/album.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './album.component.css'
 })
 export class AlbumComponent {
-
+  width: number = 170;
+  height: number = 170;
+  title: string = 'Album title';
+  price: number = 10;
+  currency: string = '$';
 }
--- Frontend-Internship-Challange/src/app/app.component.ts
+++ Frontend-Internship-Challange/src/app/app.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { NavbarComponent } from './navbar/navbar.component';
+import { AlbumComponent } from './album/album.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, AlbumComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Frontend-Internship-Challenge';
 }
- 
